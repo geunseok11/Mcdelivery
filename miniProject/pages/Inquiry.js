3b1286e63a7c404f53d2fdfe6e8a7c6549290074
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-10 22:35:21 +0900

    ADD Inquury

---
+++ miniProject/pages/Inquiry.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react';
+
+const Inquiry = (props) => {
+  return (
+    <Container>
+      <Contents></Contents>
+    </Container>
+  );
+};
+
+export default Inquiry;
