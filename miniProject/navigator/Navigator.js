50336963289b95140a0fdc6b052082a970493b60
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-10 22:34:10 +0900

    FIX navigator

--- miniProject/navigator/Navigator.js
+++ miniProject/navigator/Navigator.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../pages/Home';
+import Adress from '../pages/Adress';
+import Bookmark from '../pages/Bookmark';
+import Inquiry from '../pages/Inquiry';
+import List from '../pages/list';
+import Menu from '../pages/Menu';
 
 const Stack = createStackNavigator();
 
@@ -8,8 +13,13 @@ const Navigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Adress" component={Adress} />
+      <Stack.Screen name="Bookmark" component={Bookmark} />
+      <Stack.Screen name="Inquiry" component={Inquiry} />
+      <Stack.Screen name="List" component={List} />
+      <Stack.Screen name="Menu" component={Menu} />
     </Stack.Navigator>
   );
 };
 
-export default Navigatorl;
+export default Navigator;
