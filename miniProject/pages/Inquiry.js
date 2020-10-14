c53d5bc43517cbff8bc0b0314022f1ee89ff3314
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-14 15:56:59 +0900

    FIX inquiry

--- miniProject/pages/Inquiry.js
+++ miniProject/pages/Inquiry.js
@@ -1,9 +1,13 @@
 import React, {useEffect, useState} from 'react';
+import Nav from '../component/Nav';
+import Header from '../component/Header';
 
 const Inquiry = (props) => {
   return (
     <Container>
+      <Header props={props} />
       <Contents></Contents>
+      <Nav props={props} />
     </Container>
   );
 };
