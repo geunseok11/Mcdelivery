acb941f2fd08e95e412ac90ba367aa6e6f22e700
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-15 16:26:19 +0900

    FIX drink

---
+++ miniProject/component/drink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Image, SafeAreaView, TouchableOpacity} from 'react-native';
+
+const Drink = (props) => {
+  return (
+    <Contents>
+      <View></View>
+    </Contents>
+  );
+};
+
+export default Drink;
