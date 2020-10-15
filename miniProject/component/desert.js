cdf893a824b94dbd15d65c24746a27efd992c21f
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-15 16:25:38 +0900

    FIX desert

---
+++ miniProject/component/desert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Image, SafeAreaView, TouchableOpacity} from 'react-native';
+
+const Desert = (props) => {
+  return (
+    <Contents>
+      <View></View>
+    </Contents>
+  );
+};
+
+export default Desert;
