fe685b4981734a87634159a060b2889e7e3b7431
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-26 22:54:47 +0900

    ADD gitignore server

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const models = require('./models/index');
+// const morgan = require('morgan');
+// const path = require("path");
+// const mypage = require("./routes/mypage");
+// const user = require("./routes/user");
+// const home = require("./routes/home");
+
+const app = express();
+
+const port = 4000;
+
+app.use("/images", express.static("images"));
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// app.use("/home", home);
+// app.use("/user", user);
+// app.use("/mypage", mypage);
+
+app.listen(port, () => {
+  console.log("Server is listening to Port 4000");
+});
+
+module.exports = app;
